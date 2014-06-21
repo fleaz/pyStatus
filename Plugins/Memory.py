c49e2b5d636b9934f7fed0c6a1704327e9db9ef4
Felix Breidenstein <mail@felixbreidenstein.de>
2014-06-21 03:19:50 +0200

    Added CPU and Memory plugins

---
+++ Plugins/Memory.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+from BarItem import BarItem
+import psutil
+
+
+class Memory(BarItem):
+    output = {}
+
+    def __init__(self):
+        BarItem.__init__(self, "Memory")
+        self.output['name'] = "Memory"
+        self.update()
+
+    def update(self):
+        mem = psutil.virtual_memory()
+        mem_total = mem[0]
+        mem_percent = mem[2]
+        mem_used = mem[3]
+        mem_buffer = mem[7]
+        mem_cached = mem[8]
+
+        mem_used = mem_used - mem_buffer - mem_cached
+
+        mem_total = '%.0f' % (mem_total / 1048578)
+        mem_used = '%.0f' % (mem_used / 1048578)
+
+        self.output['full_text'] = "RAM: " + mem_used + " MB/" + mem_total + " MB [" + str(mem_percent) + "%]"
+
+    def get_json(self):
+        return self.output
--- runner.py
+++ runner.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python3
 
 from Bar import Bar
-from Plugins import Time, Battery
+from Plugins import Time, Battery, CPU, Memory
 
 
-my_bar = Bar()
-time = Time.Time("%H:%M:%S")
+my_bar = Bar(delay=3)
+time = Time.Time("%H:%M")
 bat = Battery.Battery()
+cpu = CPU.CPU()
+mem = Memory.Memory()
 
+my_bar.register_item(mem)
+my_bar.register_item(cpu)
 my_bar.register_item(bat)
 my_bar.register_item(time)
 
