cd20a039f5f13ef64a8f1e7b261edbd092535c59
Niko Wenselowski <der@nik0.de>
2014-09-07 21:15:15 +0200

    Free memory is now colored yellow if <25% free and red if less then 10%.

--- plugins/Memory.py
+++ plugins/Memory.py
@@ -40,7 +40,17 @@ class FreeMemory(BarItem):
         mem_cached = mem[8]
 
         mem_used = mem_used - mem_buffer - mem_cached
-        free_memory = ((mem_total / 1048578) - (mem_used / 1048578))
+        total_memory = (mem_total / 1048578)
+        free_memory = (total_memory - (mem_used / 1048578))
 
         self.output['full_text'] = "RAM free: {0:.0f}MB".format(free_memory)
+
+        percent_free = free_memory / total_memory * 100
+
+        if percent_free < 10:
+            self.output['color'] = '#FF0000'
+        elif percent_free < 25:
+            self.output['color'] = '#FFFF00'
+        else:
+            self.output['color'] = '#FFFFFF'
 
