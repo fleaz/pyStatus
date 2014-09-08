c4d45691106b23327c46ff8319c13c518d281de5
Felix Breidenstein <mail@felixbreidenstein.de>
2014-09-08 18:55:52 +0200

    Created an example runner.py file

---
+++ example_runner.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+
+from Bar import Bar
+from plugins import Time, Battery, CPU, Memory, MPD, Traffic, Ip, MemPercent, ESSID, MPD
+
+
+my_bar = Bar(delay=1)
+time = Time.Time("%H:%M")
+bat = Battery.Battery()
+mpd = MPD.MPD()
+cpu = CPU.CPU()
+mem = Memory.Memory()
+memPercent = MemPercent.MemoryPercent()
+traffic = Traffic.Traffic(interface="wlp3s0")
+ip = Ip.IP(interface="wlp3s0", type="wifi", protocol=4)
+ip2 = Ip.IP(interface="enp0s25", type="lan", protocol=4)
+essid = ESSID.ESSID(interface="wlp3s0")
+mpd = MPD.MPD("localhost")
+
+
+my_bar.register(essid)
+my_bar.register(ip)
+my_bar.register(ip2)
+my_bar.register(traffic)
+my_bar.register(mem)
+my_bar.register(memPercent)
+my_bar.register(cpu)
+my_bar.register(bat)
+my_bar.register(time)
+my_bar.register(mpd)
+
+my_bar.loop()
