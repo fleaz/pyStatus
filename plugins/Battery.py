cb3e0c018981a2e990101bd40e3ae48e85223f40
Felix Breidenstein <mail@felixbreidenstein.de>
2014-09-08 18:53:29 +0200

    Check if battery is installed for Batter-Plugin

--- plugins/Battery.py
+++ plugins/Battery.py
@@ -10,32 +10,36 @@ class Battery(BarItem):
         self.number = number
 
     def update(self):
-        args = ("cat", "/sys/class/power_supply/BAT0/energy_full_design") #to get a realistic value
-        popen = subprocess.Popen(args, stdout=subprocess.PIPE)
-        popen.wait()
-        bat_full = popen.stdout.read().decode("utf-8").strip()
+        try:
+            args = ("cat", "/sys/class/power_supply/BAT0/energy_full_design") #to get a realistic value
+            popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+            popen.wait()
+            bat_full = popen.stdout.read().decode("utf-8").strip()
 
-        args = ("cat", "/sys/class/power_supply/BAT0/energy_now")
-        popen = subprocess.Popen(args, stdout=subprocess.PIPE)
-        popen.wait()
-        bat_now = popen.stdout.read().decode("utf-8").strip()
+            args = ("cat", "/sys/class/power_supply/BAT0/energy_now")
+            popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+            popen.wait()
+            bat_now = popen.stdout.read().decode("utf-8").strip()
 
-        args = ("cat", "/sys/class/power_supply/BAT0/status")
-        popen = subprocess.Popen(args, stdout=subprocess.PIPE)
-        popen.wait()
-        status = popen.stdout.read().decode("utf-8").strip()
+            args = ("cat", "/sys/class/power_supply/BAT0/status")
+            popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+            popen.wait()
+            status = popen.stdout.read().decode("utf-8").strip()
 
-        percentage = (int(bat_now) / int(bat_full)) * 100
-        self.output['full_text'] = "Battery: " + '%.1f' % percentage + "%"
+            percentage = (int(bat_now) / int(bat_full)) * 100
+            self.output['full_text'] = "Battery: " + '%.1f' % percentage + "%"
 
-        if(status == "Charging"):
-            self.output['color'] = "#0676cb"
-        else:
-            if(25 < percentage):
-                self.output['color'] = "#FFFFFF"
-            elif(10 < percentage <= 25):
-                self.output['color'] = "#FFFF00"
+            if(status == "Charging"):
+                self.output['color'] = "#0676cb"
             else:
-                self.output['color'] = "#FF0000"
+                if(25 < percentage):
+                    self.output['color'] = "#FFFFFF"
+                elif(10 < percentage <= 25):
+                    self.output['color'] = "#FFFF00"
+                else:
+                    self.output['color'] = "#FF0000"
+        except:
+            self.output['color'] = "#FFFFFF"
+            self.output['full_text'] = "No battery"
 
 
