7f7ac18e9ebf13cec186ff27849295203cd1b82f
Niko Wenselowski <der@nik0.de>
2014-09-10 21:52:34 +0200

    Relative imports here too.

--- example_runner.py
+++ example_runner.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
-from Bar import Bar
-from plugins import Time, Battery, CPU, Memory, MPD, Traffic, Ip, MemPercent, ESSID, MPD
+from pyStatus.Bar import Bar
+from pyStatus.plugins import Time, Battery, CPU, Memory, MPD, Traffic, Ip, MemPercent, ESSID, MPD
 
 
 my_bar = Bar(delay=1)
