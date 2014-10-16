0c4df88278e18dd51ea4e21a052aa37bc1af381e
Niko Wenselowski <n.wenselowski@uib.de>
2014-10-16 10:35:32 +0200

    Using tuple instead of list for performance.

--- pyStatus/plugins/Filesystem.py
+++ pyStatus/plugins/Filesystem.py
@@ -26,7 +26,7 @@ class Filesystem(BarItem):
         return _ntuple_diskusage(total, used, free)
 
     def sizeof_fmt(self, num):
-        for x in ['bytes','KB','MB','GB']:
+        for x in ('bytes','KB','MB','GB'):
             if num < 1024.0:
                 return "%3.1f%s" % (num, x)
             num /= 1024.0
