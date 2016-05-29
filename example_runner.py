89f328dbca5a0a38e5bbdd168b2e082c643a3496
Felix Breidenstein <felix.breidenstein@cased.de>
2016-05-29 15:05:38 +0200

    Addd Temperatur Plugin

--- example_runner.py
+++ example_runner.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python3
 
 from pyStatus.Bar import Bar
-from pyStatus.plugins import Time, Battery, CPU, Memory, Ip, Traffic, Filesystem, ESSID, Updates, DNS, Internet, Load
+from pyStatus.plugins import Time, Battery, CPU, Memory, Ip, Traffic, Filesystem, ESSID, Updates, DNS, Internet, Load, Temp
 
 
 my_bar = Bar(delay=1, separator=False, separator_width = 15)
 my_bar.register(Updates.Updates())
+my_bar.register(Temp.Temp(warn=50, crit=70))
 my_bar.register(Filesystem.Filesystem(path='/', style="free", show_path=False))
 my_bar.register(ESSID.ESSID(interface="wlp3s0"))
 my_bar.register(Load.Load(2.0, 4.0))
